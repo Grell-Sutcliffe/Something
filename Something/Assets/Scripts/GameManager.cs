5708240e75d8bec41cb0a7e5d0cc8d23f7fd6cac
Jenny Akhtiamova <zhanna.axt@bk.ru>
2024-07-25 02:19:26 +0300

    Levels and UI

--- Something/Assets/Scripts/GameManager.cs
+++ Something/Assets/Scripts/GameManager.cs
@@ -1,9 +1,16 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public static event Action OnScoreChanged;
+    [SerializeField] private LevelPreviewData[] _levels;
+    private int _currentLevelIndex;
+    public int Score { 
+        get { return _score; } 
+    }
     private int _score;
 
     private void Awake()
@@ -23,6 +30,24 @@ public class GameManager : MonoBehaviour
                 $"Amount should be positive: {gameObject.name}"
                 );
         _score += amount;
+        OnScoreChanged?.Invoke();
         Debug.Log($"Score: {_score}");
+    }
+
+    public void FinishCurrentLevel()
+    {
+        SceneManager.LoadScene(0);
+        OpenAccessToNextLevel();
+    }
+
+    public void OpenAccessToNextLevel()
+    {
+        if (_currentLevelIndex + 1 != _levels.Length)
+            _levels[_currentLevelIndex + 1].IsAccessable = true;
+    }
+
+    public void SetLevelIndex(int newIndex)
+    {
+        _currentLevelIndex = newIndex;
     }
 }
---
+++ Something/Assets/Scripts/UI/LevelPreviewData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelPreviewData", menuName = "Levels/Preview")]
+public class LevelPreviewData : ScriptableObject
+{
+    public Sprite Preview;
+    public string Name;
+    public int SceneIndex;
+    public bool IsAccessable;
+}
